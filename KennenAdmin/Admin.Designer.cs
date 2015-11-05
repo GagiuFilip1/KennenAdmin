8ad631e1ec50643e40ffe010a1def6e26b7c3e63
GagiuFilip1 <gagiu.filip@gmail.com>
2015-11-06 00:37:14 +0200

    KennenAdmin
    Stream

---
+++ KennenAdmin/Admin.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace KennenAdmin
+{
+    partial class Admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Admin));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sendURLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sendToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.backToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(0, 26);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(863, 399);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated_1);
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Location = new System.Drawing.Point(0, -137);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(863, 562);
+            this.webBrowser2.TabIndex = 1;
+            this.webBrowser2.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser2_Navigated);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.button2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button2.Location = new System.Drawing.Point(837, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(26, 27);
+            this.button2.TabIndex = 23;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_2);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(18)))), ((int)(((byte)(35)))));
+            this.checkBox1.ForeColor = System.Drawing.Color.Black;
+            this.checkBox1.Location = new System.Drawing.Point(610, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 17);
+            this.checkBox1.TabIndex = 22;
+            this.checkBox1.Text = "Use Proxy";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged_2);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(18)))), ((int)(((byte)(35)))));
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.sendURLToolStripMenuItem,
+            this.textBox1,
+            this.backToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(863, 27);
+            this.menuStrip1.TabIndex = 21;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(63)))));
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 23);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // sendURLToolStripMenuItem
+            // 
+            this.sendURLToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(63)))));
+            this.sendURLToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.sendToolStripMenuItem});
+            this.sendURLToolStripMenuItem.Name = "sendURLToolStripMenuItem";
+            this.sendURLToolStripMenuItem.Size = new System.Drawing.Size(69, 23);
+            this.sendURLToolStripMenuItem.Text = "Send URL";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click_2);
+            // 
+            // sendToolStripMenuItem
+            // 
+            this.sendToolStripMenuItem.Name = "sendToolStripMenuItem";
+            this.sendToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.sendToolStripMenuItem.Text = "Send";
+            this.sendToolStripMenuItem.Click += new System.EventHandler(this.sendToolStripMenuItem_Click_2);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(63)))));
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(400, 23);
+            // 
+            // backToolStripMenuItem
+            // 
+            this.backToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(63)))));
+            this.backToolStripMenuItem.Name = "backToolStripMenuItem";
+            this.backToolStripMenuItem.Size = new System.Drawing.Size(59, 23);
+            this.backToolStripMenuItem.Text = "LogOut";
+            this.backToolStripMenuItem.Click += new System.EventHandler(this.backToolStripMenuItem_Click_1);
+            // 
+            // Admin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 426);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.webBrowser2);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Admin";
+            this.Text = "Admin";
+            this.Load += new System.EventHandler(this.Admin_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sendURLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sendToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem backToolStripMenuItem;
+
+    }
+}
---
+++ KennenAdmin/Admin.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Nemiro.OAuth;
+using Nemiro.OAuth.LoginForms;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace KennenAdmin
+{
+    public partial class Admin : Form
+    {
+        public Admin()
+        {
+            InitializeComponent();
+        }
+
+        private void Admin_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Text = String.Empty;
+            webBrowser2.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(browser_DocumentCompleted);
+            webBrowser1.Navigate(new Uri("https://www.youtube.com/"));
+            webBrowser2.Hide();
+        }
+        private void browser_NavigateError(object sender, WebBrowserNavigatingEventArgs e) //WebBrowserNavigateErrorEventArgs
+        {
+            if (webBrowser2.ScriptErrorsSuppressed)
+            {
+                webBrowser2.DocumentCompleted -= browser_DocumentCompleted;
+                webBrowser2.ScriptErrorsSuppressed = false;
+                webBrowser2.Stop();
+                webBrowser2.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(browser_DocumentCompleted_Error);
+            }
+        }
+        private void browser_DocumentCompleted_Error(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+            webBrowser2.DocumentCompleted -= browser_DocumentCompleted_Error;
+            webBrowser2.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(browser_DocumentCompleted);
+
+            webBrowser2.Navigate(new Uri("http://www.hideoxy.com/service/page.php?u=Z80qqYl1puDoTfVeYq3pP0uzahE%3D&b=5"));
+
+        }
+
+        private void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs args)
+        {
+            webBrowser2.ScriptErrorsSuppressed = true;
+            
+        }
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            textBox1.Text = webBrowser1.Url.ToString();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Information info = new Information();
+                info.Url = textBox1.Text;
+                SaveXML.SaveData(info, "Data.xml");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void sendToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            XDocument doc;
+            string appPath = Path.GetDirectoryName(Application.ExecutablePath);
+            doc = XDocument.Load("Data.xml");
+            doc.Save(appPath + "\\" + "Data.xml");
+            FileStream upstream = new FileStream((appPath + "\\" + "Data.xml").ToString(), FileMode.Open);
+            OAuthUtility.PutAsync
+                         (
+                         "https://api-content.dropbox.com/1/files_put/auto//",
+                         new HttpParameterCollection
+                {
+                    {"access_token",Properties.Settings.Default.AccessToken},
+                    {"path",Path.Combine(Path.GetFileName("Data.xml")).Replace("\\","/")},
+                    {"overwrite","true"},
+                    {"autorename","true"},
+                    {upstream}
+                },
+                         callback: Upload_Result
+                         );
+            link();
+        }
+        private void Upload_Result(RequestResult result)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action<RequestResult>(Upload_Result), result);
+                return;
+
+            }
+        }
+        private void link()
+        {
+
+            OAuthUtility.PostAsync
+            (
+              "https://api.dropbox.com/1/shares/auto/",
+                new HttpParameterCollection
+                {
+                    {"path",Path.Combine(Path.GetFileName("Data.xml")).Replace("\\","/") },
+                    {"access_token",Properties.Settings.Default.AccessToken },
+                    {"short_url","false"}
+                },
+                callback: GetShareLink_Result
+             );
+        }
+        private void GetShareLink_Result(RequestResult result)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void backToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                webBrowser2.Show();
+                webBrowser2.Navigate(new Uri("http://de.proxfree.com/permalink.php?url=s54aaHA%2FaGsaD5vHs0QZLcUclS%2FcUxrnNZwEMCiA0I8%3D&bit=1"));
+            }
+            if(checkBox1.Checked == false)
+            {
+                webBrowser1.Navigate(new Uri("https://www.youtube.com/"));
+            }
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sendURLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+
+                webBrowser1.Navigate(new Uri("http://de.proxfree.com/permalink.php?url=s54aaHA%2FaGsaD5vHs0QZLcUclS%2FcUxrnNZwEMCiA0I8%3D&bit=1"));
+            }
+            if (checkBox1.Checked == false)
+            {
+                webBrowser1.Navigate(new Uri("http://de.proxfree.com/permalink.php?url=s54aaHA%2FaGsaD5vHs0QZLcUclS%2FcUxrnNZwEMCiA0I8%3D&bit=1"));
+            }
+        }
+
+        private void saveToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                Information info = new Information();
+                info.Url = textBox1.Text;
+                SaveXML.SaveData(info, "Data.xml");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void sendToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+
+            XDocument doc;
+            string appPath = Path.GetDirectoryName(Application.ExecutablePath);
+            doc = XDocument.Load("Data.xml");
+            doc.Save(appPath + "\\" + "Data.xml");
+            FileStream upstream = new FileStream((appPath + "\\" + "Data.xml").ToString(), FileMode.Open);
+            OAuthUtility.PutAsync
+                         (
+                         "https://api-content.dropbox.com/1/files_put/auto//",
+                         new HttpParameterCollection
+                {
+                    {"access_token",Properties.Settings.Default.AccessToken},
+                    {"path",Path.Combine(Path.GetFileName("Data.xml")).Replace("\\","/")},
+                    {"overwrite","true"},
+                    {"autorename","true"},
+                    {upstream}
+                },
+                         callback: Upload_Result
+                         );
+            link();
+        }
+
+        private void checkBox1_CheckedChanged_2(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                
+                webBrowser1.Hide();
+                webBrowser2.Show();
+                webBrowser2.ScriptErrorsSuppressed = false;
+                webBrowser2.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(browser_DocumentCompleted);
+                webBrowser2.Navigate(new Uri("http://www.hideoxy.com/service/page.php?u=Z80qqYl1puDoTfVeYq3pP0uzahE%3D&b=5"));
+            }
+            if (checkBox1.Checked == false)
+            {
+                
+                webBrowser2.Hide();
+                webBrowser1.Show();
+                webBrowser1.Navigate(new Uri("https://www.youtube.com/"));
+            }
+        }
+
+        private void webBrowser2_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            textBox1.Text = webBrowser2.Url.ToString();
+        }
+
+        private void button2_Click_2(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void backToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveToolStripMenuItem_Click_2(object sender, EventArgs e)
+        {
+            try
+            {
+                Information info = new Information();
+                info.Url = textBox1.Text;
+                SaveXML.SaveData(info, "Data.xml");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void sendToolStripMenuItem_Click_2(object sender, EventArgs e)
+        {
+            XDocument doc;
+            string appPath = Path.GetDirectoryName(Application.ExecutablePath);
+            doc = XDocument.Load("Data.xml");
+            doc.Save(appPath + "\\" + "Data.xml");
+            FileStream upstream = new FileStream((appPath + "\\" + "Data.xml").ToString(), FileMode.Open);
+            OAuthUtility.PutAsync
+                         (
+                         "https://api-content.dropbox.com/1/files_put/auto//",
+                         new HttpParameterCollection
+                {
+                    {"access_token",Properties.Settings.Default.AccessToken},
+                    {"path",Path.Combine(Path.GetFileName("Data.xml")).Replace("\\","/")},
+                    {"overwrite","true"},
+                    {"autorename","true"},
+                    {upstream}
+                },
+                         callback: Upload_Result
+                         );
+            link();
+        }
+
+        private void webBrowser1_Navigated_1(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            textBox1.Text = webBrowser1.Url.ToString();
+        }
+    }
+}
